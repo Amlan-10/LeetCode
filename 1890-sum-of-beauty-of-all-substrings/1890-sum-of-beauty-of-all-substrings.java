5dbff5d95f6c22617d9e16dc9b762eab38af84b3
Amlan Tribedi <92737628+Amlan-10@users.noreply.github.com>
2024-08-01 18:56:38 +0530

    Time: 56 ms (91.8%), Space: 42.8 MB (46.69%) - LeetHub

---
+++ 1890-sum-of-beauty-of-all-substrings/1890-sum-of-beauty-of-all-substrings.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int beautySum(String s) {
+        int n = s.length();
+        int totalBeauty = 0;
+        
+        for (int i = 0; i < n; i++) {
+            int[] hash = new int[26];
+            for (int j = i; j < n; j++) {
+                hash[s.charAt(j) - 'a']++;
+                int max = 0, min = Integer.MAX_VALUE;
+                for (int k = 0; k < 26; k++) {
+                    if (hash[k] > 0) {
+                        max = Math.max(max, hash[k]);
+                        min = Math.min(min, hash[k]);
+                    }
+                }
+                totalBeauty += max - min;
+            }
+        }
+        
+        return totalBeauty;
+    }
+}
