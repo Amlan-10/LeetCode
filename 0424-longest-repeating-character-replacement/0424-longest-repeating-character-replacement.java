ad625ab981c996201e62fd85572a1d3148aa9b9d
Amlan Tribedi <92737628+Amlan-10@users.noreply.github.com>
2024-08-03 02:47:38 +0530

    Time: 10 ms (63.41%), Space: 42.4 MB (60.7%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int n=s.length();
+        if(n==0){
+            return 0;
+        }
+        int [] hash=new int[26];
+        Arrays.fill(hash,0);
+        int l=0,r=0,maxfreq=0,maxlen=0;
+        while(r<n){
+            hash[s.charAt(r)-'A']++;
+            maxfreq=Math.max(maxfreq,hash[s.charAt(r)-'A']);
+            if(r-l+1-maxfreq>k){
+                hash[s.charAt(l)-'A']--;
+                l++;
+            }
+            if(r-l+1-maxfreq<=k){
+                maxlen=Math.max(maxlen,r-l+1);
+            }
+            r++;
+        }
+        return maxlen;
+    }
+}
