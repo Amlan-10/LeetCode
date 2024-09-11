c43f72b22eaef5ae42055c23e46618c8f32f5c7e
Amlan Tribedi <92737628+Amlan-10@users.noreply.github.com>
2024-09-11 18:27:12 +0530

    Time: 22 ms (39.43%), Space: 44.8 MB (34.64%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int n=coins.length;
+        int []dp=new int[10010];
+        Arrays.fill(dp,-1);
+        int ans=helper(amount,coins,dp);
+        return ans==Integer.MAX_VALUE/2?-1:ans;
+    }
+    static int helper(int amount,int []coins,int []dp){
+        if(amount==0) return 0;
+        if(dp[amount]!=-1) return dp[amount];
+        int ans=Integer.MAX_VALUE/2;
+        for(int coin:coins){
+            if(amount-coin>=0){
+                ans=Math.min(ans,helper(amount-coin,coins,dp)+1);
+            }
+        }
+        return dp[amount]=ans;
+    }
+}
