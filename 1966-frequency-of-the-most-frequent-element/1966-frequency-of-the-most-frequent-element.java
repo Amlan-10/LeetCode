7ca425d503ce24330f6f1d3d5ad96c330cb7158e
Amlan Tribedi <92737628+Amlan-10@users.noreply.github.com>
2024-09-13 14:07:39 +0530

    Time: 31 ms (19.6%), Space: 56.1 MB (94.93%) - LeetHub

---
+++ 1966-frequency-of-the-most-frequent-element/1966-frequency-of-the-most-frequent-element.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxFrequency(int[] nums, int k) {
+        Arrays.sort(nums);
+        int n=nums.length;
+        int res=0;
+        int l=0;
+        long cursum=0;
+        for(int r=0;r<n;r++){
+            long target=nums[r];
+            cursum+=nums[r];
+            if((r-l+1)*target-cursum>k){
+                cursum-=nums[l];
+                l++;
+            }
+            res=Math.max(res,r-l+1);
+        }
+        return (int)res;
+    }
+}
