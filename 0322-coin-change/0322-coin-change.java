8a944f76a254d780390969f7b3e178795265286b
Amlan Tribedi <92737628+Amlan-10@users.noreply.github.com>
2024-09-14 18:28:29 +0530

    Time: 74 ms (11.48%), Space: 45.3 MB (18.73%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,20 +1,32 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int n=coins.length;
-        int []dp=new int[amount+1];
-        Arrays.fill(dp,-1);
-        int ans=helper(amount,coins,dp);
-        return ans==Integer.MAX_VALUE/2?-1:ans;
+        int [][] dp=new int[n+1][amount+1];
+        for(int [] num:dp){
+            Arrays.fill(num,-1);
+        }
+       int ans=helper(coins,n-1,amount,dp);
+       if(ans>=(int) Math.pow(10, 9)){
+        return -1;
+       }
+       return ans;
     }
-    static int helper(int amount,int []coins,int []dp){
-        if(amount==0) return 0;
-        if(dp[amount]!=-1) return dp[amount];
-        int ans=Integer.MAX_VALUE/2;
-        for(int coin:coins){
-            if(amount-coin>=0){
-                ans=Math.min(ans,helper(amount-coin,coins,dp)+1);
+    static int helper(int []a,int ind,int tar,int [][]dp){
+        if(ind==0){
+            if(tar%a[ind]==0){
+                return tar/a[ind];
+            }else{
+                return (int) Math.pow(10, 9);
             }
         }
-        return dp[amount]=ans;
+        if(dp[ind][tar]!=-1){
+            return dp[ind][tar];
+        }
+        int nottaken=helper(a,ind-1,tar,dp);
+        int taken=(int) Math.pow(10, 9);
+        if(a[ind]<=tar){
+            taken=1+helper(a,ind,tar-a[ind],dp);
+        }
+        return dp[ind][tar]=Math.min(taken,nottaken);
     }
 }
