141cf1941b9e0cd075e60a62bddaf144724397df
Amlan Tribedi <92737628+Amlan-10@users.noreply.github.com>
2024-11-17 12:30:05 +0530

    Time: 3 ms (89.97%), Space: 42.8 MB (86.04%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isAnagram(String str1, String str2) {
+        if (str1.length() != str2.length())
+            return false;
+
+        int[] freq = new int[26];
+        for (int i = 0; i < str1.length(); i++) {
+            freq[str1.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < str2.length(); i++) {
+            freq[str2.charAt(i) - 'a']--;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] != 0)
+                return false;
+        }
+        return true;
+    }
+}
