7a08c342906016a1be20b6896e55df7922273115
Amlan Tribedi <92737628+Amlan-10@users.noreply.github.com>
2024-11-25 23:10:13 +0530

    Time: 16 ms (82.83%), Space: 43.6 MB (91.08%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,41 +1,41 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        int n=nums.length;
         Arrays.sort(nums);
+        int n=nums.length;
         List<List<Integer>> ans=new ArrayList<>();
         for(int i=0;i<n;i++){
             if(i>0 && nums[i]==nums[i-1]){
                 continue;
             }
             for(int j=i+1;j<n;j++){
-                int k=j+1;
-                int l=n-1;
                 if(j>i+1 && nums[j]==nums[j-1]){
                     continue;
                 }
+                
+                int k=j+1;
+                int l=n-1;
                 while(k<l){
-                    long sum=nums[i];
-                    sum += nums[j];
-                    sum += nums[k];
-                    sum += nums[l];
-                    if(sum<target){
-                        k++;
-                    }
-                    else if(sum>target){
-                        l--;
-                    }else{
-                        List<Integer> list=new ArrayList<>();
-                        list.add(nums[i]);
-                        list.add(nums[j]);
-                        list.add(nums[k]);
-                        list.add(nums[l]);
-                        // if(list.size()!=0)
-                        ans.add(list);
-                        k++;
-                        l--;
-                        while(k<l && nums[k]==nums[k-1]){k++;}
-                        while(k<l && nums[l]==nums[l+1]){l--;}
-                    }
+                long sum=nums[i];
+                sum+=nums[j];
+                sum+=nums[k];
+                sum+=nums[l];
+                if(sum<target){
+                    k++;
+                }else if(sum>target){
+                    l--;
+                }
+                else{
+                    List<Integer> a=new ArrayList<>();
+                    a.add(nums[i]);
+                    a.add(nums[j]);
+                    a.add(nums[k]);
+                    a.add(nums[l]);
+                    ans.add(a);
+                    k++;
+                    l--;
+                    while(k<l && nums[k]==nums[k-1]) k++;
+                    while(k<l && nums[l]==nums[l+1]) l--;
+                }
                 }
             }
         }
