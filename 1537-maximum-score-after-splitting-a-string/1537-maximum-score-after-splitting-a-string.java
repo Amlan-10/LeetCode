1fa675b0e53cc7543d5486d228a8c2e267d13e32
Amlan Tribedi <92737628+Amlan-10@users.noreply.github.com>
2025-01-03 16:30:58 +0530

    Time: 140 ms (5.82%), Space: 41.5 MB (53.47%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.java
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.java
@@ -1,38 +1,27 @@
 class Solution {
-    static int countZero(String s){
-        int n=s.length();
-        int cnt=0;
-        for(int i=0;i<n;i++){
-            if(s.charAt(i)=='0'){
-                cnt++;
-            }
-        }
-        return cnt;
-    }
-    static int countOnes(String s){
-        int n=s.length();
-        int cnt=0;
-        for(int i=0;i<n;i++){
-            if(s.charAt(i)=='1'){
-                cnt++;
-            }
-        }
-        return cnt;
-    }
     public int maxScore(String s) {
         int n = s.length();
-        String l = "";
-        String r = "";
-        int max = Integer.MIN_VALUE;
-        int zeroes = 0, ones = 0, sum = 0;
-        for (int i = 0; i < n-1; i++) {
-            l = s.substring(0, i + 1);
-            r = s.substring(i + 1, n);
-            zeroes = countZero(l);
-            ones = countOnes(r);
-            sum=zeroes+ones;
-            max=Math.max(max,sum);
+        int leftZeroes = 0, rightOnes = 0;
+        
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == '1') {
+                rightOnes++;
+            }
         }
+        
+        int max = Integer.MIN_VALUE;
+       
+        for (int i = 0; i < n - 1; i++) {
+            if (s.charAt(i) == '0') {
+                leftZeroes++;
+            } else {
+                rightOnes--;
+            }
+            
+            int sum = leftZeroes + rightOnes;
+            max = Math.max(max, sum);
+        }
+        
         return max;
     }
 }
