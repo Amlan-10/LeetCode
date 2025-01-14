abf30e4459694528829b3c004797aaf228d0b531
Amlan Tribedi <92737628+Amlan-10@users.noreply.github.com>
2025-01-14 11:00:56 +0530

    Time: 80 ms (21.34%), Space: 44 MB (90.12%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -3,18 +3,18 @@ class Solution {
         int n=nums.length;
         int []dp=new int[n];
         Arrays.fill(dp,-1);
-        int ans=0;
+        int max=Integer.MIN_VALUE;
         for(int i=0;i<n;i++){
-            ans=Math.max(ans,helper(i,nums,dp));
+            max=Math.max(max,lis(i,nums,dp));
         }
-        return ans;
+        return max;
     }
-    static int helper(int i,int []a,int []dp){
+    public static int lis(int i,int []nums,int []dp){
         int ans=1;
         if(dp[i]!=-1) return dp[i];
         for(int j=0;j<i;j++){
-            if(a[i]>a[j]){
-                ans=Math.max(ans,helper(j,a,dp)+1);
+            if(nums[i]>nums[j]){
+                ans=Math.max(ans,lis(j,nums,dp)+1);
             }
         }
         return dp[i]=ans;
